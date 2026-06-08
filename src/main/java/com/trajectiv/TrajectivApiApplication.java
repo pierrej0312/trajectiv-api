aa4432ebc9acd6df1447cb3ff0dc1de3cfe0760e
Pierre Juif <pierrej0312@gmail.com>
2026-06-08 11:24:46 +0200

    first commit

---
+++ src/main/java/com/trajectiv/TrajectivApiApplication.java
@@ -0,0 +1,13 @@
+package com.trajectiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrajectivApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrajectivApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trajectiv/TrajectivApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trajectiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrajectivApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
